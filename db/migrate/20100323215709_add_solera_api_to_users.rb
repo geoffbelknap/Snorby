434a52f8d62efa4b7cc6cb8f1bde8c6101f6c512
fracBlend <geoff.belknap@gmail.com>
2010-03-24 11:36:11 -0600

    Updated Users Migrate

--- db/migrate/20100323215709_add_solera_api_to_users.rb
+++ db/migrate/20100323215709_add_solera_api_to_users.rb
@@ -2,10 +2,12 @@ class AddSoleraApiToUsers < ActiveRecord::Migration
   def self.up
     add_column :users, :solera_api_user, :string
     add_column :users, :solera_api_pass, :string
+    add_column :users, :solera_api_enabled, :integer
   end
 
   def self.down
     remove_column :users, :solera_api_pass
     remove_column :users, :solera_api_user
+    remove_column :users, :solera_api_enabled
   end
 end
